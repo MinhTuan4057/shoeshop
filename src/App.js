182adcafacdb003e467682ef3ac551fa9dbb5e1a
Minh Tuan <nguyenminhtuan4057@gmail.com>
2023-08-02 20:51:18 +0700

    shoeshop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import BTShoeShop from './BTShoeShop/BTShoeShop';
+
+
+//component lớn nhất trong dự án
+
+// 2 loại component
+// + class component (stateful) <2019 (lifecyle)
+// + function component (stateless) 100%
+// 1 component chỉ đc phép return về 1 thẻ jsx duy nhất
+// attribute: viết theo quy tác camelCase
+
+//JSX: Javascript XML => giúp các bạn viết html trong javascript
 
 function App() {
   return (
+    // <div></div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {/* <span className='title'>HELLO BC47</span>
+    <p></p>
+    <h1></h1>
+
+    <FunctionComponent></FunctionComponent>
+    <ClassComponent></ClassComponent> */}
+
+    <BTShoeShop></BTShoeShop>
+
+</div>
   );
 }
 
